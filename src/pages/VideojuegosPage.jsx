4053951925e16b2a1c4b3a094f90608a30fea8b4
Rafael Lau <64998960+rafaellau17@users.noreply.github.com>
2026-01-27 15:33:04 -0500

    Update VideojuegosPage.jsx

--- src/pages/VideojuegosPage.jsx
+++ src/pages/VideojuegosPage.jsx
@@ -2,7 +2,7 @@ import GrillaVideojuegos from "../components/GrillaVideojuegos"
 import Titulo from "../components/Titulo"
 import Filtro from "../components/Filtro"
 import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { Navigate, useNavigate } from "react-router-dom"
 
 function VideojuegosPage() {
     const categorias = [
@@ -11,26 +11,19 @@ function VideojuegosPage() {
 
     const navigate = useNavigate()
 
-    async function obtenerVideojuegosHTTP() {
-        const URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLiLtCG2QO9ojxUcwqiSou_YnRe3yyq3zvGrhqw80lxUiRfBl-LxTV6Q6d_2lm_LFf5rD7u9xxqswpEuZmshZMn87TPsnQRAssUqglCgkxdHxEdYPufwoVCoIHEKOHczAbZacinsCm2Tgv2l0FxSij4RCt_cevSXUWcrRY5_d4KVr5WmYVK77ng2D5JS_ao68-6_QOnjcM97bsMZW0GaW2pvQMUOHHJYrV054blJHpB0T-DNGhWUKtB0iZrqpiwn4auxIXNZyxzNHliuu0S-lZt7xODbp-O33hdCqLDa&lib=MwotjRmUun0RLlzJNoicmGhJptMVnD4LO"
+    const [listaVideojuegos, setListaVideojuegos] = useState([])
+
+    (async function obtenerVideojuegosHTTP() {
+        const URL = "https://script.google.com/macros/s/AKfycbxMZbg2ZTtWjfgmRVP25A2Kt6i02_SDLcu1asfc9CKNXDxLISrTxqaoK5pdgBrjmc1Ijw/exec"
         const response = await fetch(URL)
+
+        if(!response.ok) {
+            console.error("Error de peticion" + response.status)
+            return
+        }
         const data = await response.json()
-        console.log(data)
-    }
-
-    const lista = [{
-        nombre: "CSGO",
-        imagen: "/imagenes/csgo.jpg",
-        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor eros sed semper aliquam. Duis quis tellus libero. Quisque consequat mauris eu molestie vestibulum. Nunc at ligula aliquet, accumsan dui ac, lacinia nulla. Quisque in aliquam nisl. Maecenas faucibus est orci, non facilisis arcu vestibulum ut. Nulla congue dapibus sagittis. Pellentesque et lectus at ante convallis tristique vitae in dolor. Donec varius mauris id enim cursus ultrices. Sed a lobortis quam.",
-        categoria: "FPS"
-    }, {
-        nombre: "Honkai Star Rail",
-        imagen: "https://lutris.net/media/games/screenshots/fef9f3e2275119630707c14817fd4db7_438971071424613688.png",
-        descripcion: "Un videojuego de rol de fantasía espacial donde los jugadores viajan por la galaxia en el Expreso Astral, similar a Genshin Impact pero por turnos y enfocado en la exploración cósmica y la resolución de misterios estelares.",
-        categoria: "RPG"
-    }]
-
-    const [listaVideojuegos, setListaVideojuegos] = useState(lista)
+        setListaVideojuegos(data)
+    })()
 
     function filtrar(categoria) {
         if (categoria == "-1") {
