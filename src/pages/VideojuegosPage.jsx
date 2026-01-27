c53a38d8c211c012030bf2d3961e114e3d5177d6
Rafael Lau <64998960+rafaellau17@users.noreply.github.com>
2026-01-27 15:48:53 -0500

    Arreglo UseEffect Fetch

--- src/pages/VideojuegosPage.jsx
+++ src/pages/VideojuegosPage.jsx
@@ -1,8 +1,8 @@
 import GrillaVideojuegos from "../components/GrillaVideojuegos"
 import Titulo from "../components/Titulo"
 import Filtro from "../components/Filtro"
-import { useState } from "react"
-import { Navigate, useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 function VideojuegosPage() {
     const categorias = [
@@ -13,21 +13,24 @@ function VideojuegosPage() {
 
     const [listaVideojuegos, setListaVideojuegos] = useState([])
 
-    (async function obtenerVideojuegosHTTP() {
-        const URL = "https://script.google.com/macros/s/AKfycbxMZbg2ZTtWjfgmRVP25A2Kt6i02_SDLcu1asfc9CKNXDxLISrTxqaoK5pdgBrjmc1Ijw/exec"
-        const response = await fetch(URL)
+    useEffect(function () {
+        async function obtenerVideojuegosHTTP() {
+            const URL = "https://script.google.com/macros/s/AKfycbxMZbg2ZTtWjfgmRVP25A2Kt6i02_SDLcu1asfc9CKNXDxLISrTxqaoK5pdgBrjmc1Ijw/exec"
+            const response = await fetch(URL)
 
-        if(!response.ok) {
-            console.error("Error de peticion" + response.status)
-            return
+            if (!response.ok) {
+                console.error("Error de peticion" + response.status)
+                return
+            }
+            const data = await response.json()
+            setListaVideojuegos(data)
         }
-        const data = await response.json()
-        setListaVideojuegos(data)
-    })()
+        obtenerVideojuegosHTTP();
+    }, [])
 
     function filtrar(categoria) {
         if (categoria == "-1") {
-            setListaVideojuegos(lista)
+            setListaVideojuegos(listaVideojuegos)
         }
         else {
             const listaVideojuegosModificada = listaVideojuegos.filter(function (vj) {
@@ -43,7 +46,7 @@ function VideojuegosPage() {
     }
 
     return <div className="px-4">
-        <Titulo onLogout={logout}/>
+        <Titulo onLogout={logout} />
         <Filtro categorias={categorias} onFiltro={filtrar} />
         <hr className="mb-4" />
         <GrillaVideojuegos listaVideojuegos={listaVideojuegos} />
