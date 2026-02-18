961e6110d2f6743e63e0fe6df733a3e6ad70838a
Rafael Lau <64998960+rafaellau17@users.noreply.github.com>
2026-02-18 15:47:10 -0500

    imagen import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './main.css'
 import LoginPage from './pages/LoginPage'
 import VideojuegosPage from './pages/VideojuegosPage'
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
